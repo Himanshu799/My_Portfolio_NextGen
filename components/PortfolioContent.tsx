10793b057f3d241e9047a9f86d638bca0c672844
himanshuk79 <kashyaph92@gmail.com>
2025-12-30 08:39:20 +0000

    Fix Sanity dataset env

--- components/PortfolioContent.tsx
+++ components/PortfolioContent.tsx
@@ -1,16 +1,10 @@
 import {
   AboutSection,
-  AchievementsSection,
   BlogSection,
-  CertificationsSection,
   ContactSection,
   EducationSection,
-  ExperienceSection,
   HeroSection,
   ProjectsSection,
-  ServicesSection,
-  SkillsSection,
-  TestimonialsSection,
 } from "@/components/sections";
 
 async function PortfolioContent() {
